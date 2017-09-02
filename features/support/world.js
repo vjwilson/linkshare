9c58a34cb8a0fb53b2da7c9703187ab08755d28f
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2017-09-02 13:46:12 -0400

    Add cucumber.js setup for functional testing

---
+++ features/support/world.js
@@ -0,0 +1,14 @@
+require('chromedriver')
+var seleniumWebdriver = require('selenium-webdriver');
+var {defineSupportCode} = require('cucumber');
+
+function CustomWorld() {
+  this.driver = new seleniumWebdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+}
+
+defineSupportCode(function({setWorldConstructor}) {
+  setWorldConstructor(CustomWorld)
+})
+
