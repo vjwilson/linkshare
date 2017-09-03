e14e6e9b7d670e1cc313d8c53ccdbd39bb645945
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2017-09-03 10:05:01 -0400

    Add second functional test for visiting a link url

--- features/documentation.feature
+++ features/documentation.feature
@@ -7,3 +7,9 @@ Feature: List of links
     Given I am on the Linkshare homepage
     When I look for a list of links
     Then I should see one or more link elements
+
+  Scenario: Visiting a given link
+    Given I am on the Linkshare homepage
+    When I look for a list of links
+    And I click the link text in the first link item
+    Then I should visit the site in the link item href
--- features/step_definitions/browser_steps.js
+++ features/step_definitions/browser_steps.js
@@ -4,6 +4,7 @@ var assert = require('assert');
 
 defineSupportCode(function({Given, When, Then}) {
   var links;
+  var linkUrlToTest;
 
   Given('I am on the Linkshare homepage', function() {
     return this.driver.get('http://localhost:3000');
@@ -17,5 +18,18 @@ defineSupportCode(function({Given, When, Then}) {
 
   Then('I should see one or more link elements', function () {
     assert(links.length, 4);
+  });
+
+  When('I click the link text in the first link item', function () {
+    return links[0].findElement({css: '.link-item-link'}).then(function(element) {
+      linkUrlToTest = element.getAttribute('href');
+      element.click();
+    });
+  });
+
+  Then('I should visit the site in the link item href', function () {
+    return this.driver.getCurrentUrl().then(function(url) {
+      assert(url, linkUrlToTest);
+    });
   });
 });
