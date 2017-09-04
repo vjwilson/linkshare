e60c897b9618ebedc9b2420ed0079bf5ea2b9fd3
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2017-09-04 08:43:06 -0400

    Add third functional test for a favorite count

--- features/documentation.feature
+++ features/documentation.feature
@@ -13,3 +13,11 @@ Feature: List of links
     When I look for a list of links
     And I click the link text in the first link item
     Then I should visit the site in the link item href
+
+  Scenario: Adding to the favorite count of a link
+    Given I am on the Linkshare homepage
+    When I look for a list of links
+    And I focus on the link at position 2
+    And I save the favorite count of the focused link
+    And I click the Add Favorite button of the focused link
+    Then The favorite count of the link I found should go up by 1
--- features/step_definitions/browser_steps.js
+++ features/step_definitions/browser_steps.js
@@ -5,6 +5,8 @@ var assert = require('assert');
 defineSupportCode(function({Given, When, Then}) {
   var links;
   var linkUrlToTest;
+  var focusedLink;
+  var existingFavoriteCount;
 
   Given('I am on the Linkshare homepage', function() {
     return this.driver.get('http://localhost:3000');
@@ -17,19 +19,51 @@ defineSupportCode(function({Given, When, Then}) {
   });
 
   Then('I should see one or more link elements', function () {
-    assert(links.length, 4);
+    assert.equal(links.length, 4);
   });
 
   When('I click the link text in the first link item', function () {
     return links[0].findElement({css: '.link-item-link'}).then(function(element) {
-      linkUrlToTest = element.getAttribute('href');
-      element.click();
+      linkUrlToTest = element.getAttribute('href').then(function(href) {
+        linkUrlToTest = href;
+        element.click();
+      });
     });
   });
 
   Then('I should visit the site in the link item href', function () {
     return this.driver.getCurrentUrl().then(function(url) {
-      assert(url, linkUrlToTest);
+      assert.equal(url, linkUrlToTest);
+    });
+  });
+
+  When('I focus on the link at position {int}', function (position) {
+    var arrayIndex = position - 1;
+    return this.driver.findElements({css: '.link-list .link-item'}).then(function(elements) {
+      focusedLink = elements[arrayIndex];
+    });
+  });
+
+  When('I save the favorite count of the focused link', function () {
+    return focusedLink.findElement({css: '.fave-count'}).then(function(element) {
+      element.getText().then(function(faveCountText) {
+        existingFavoriteCount = parseInt(faveCountText);
+      });
+    });
+  });
+
+  When('I click the Add Favorite button of the focused link', function () {
+    return focusedLink.findElement({css: '.add-fave'}).then(function(element) {
+      element.click();
+    });
+  });
+
+  Then('The favorite count of the link I found should go up by 1', function () {
+    return focusedLink.findElement({css: '.fave-count'}).then(function(element) {
+      newFavoriteCount = element.getText().then(function(faveCountText) {
+        var newFavoriteCount = parseInt(faveCountText);
+        assert.equal(newFavoriteCount, existingFavoriteCount + 1);
+      });
     });
   });
 });
